9aa476d901eb3ec4ffad1fd241e0caf1d1d88f90
Juliano Boese <juliano.boese@gmail.com>
2022-03-10 16:23:40 -0300

    Requisito 15 - botão de reiniciar o jogo

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 export default class Feedback extends Component {
@@ -7,6 +8,9 @@ export default class Feedback extends Component {
       <>
         <Header />
         <span data-testid="feedback-text">Xablau</span>
+        <Link to="/">
+          <button type="button" data-testid="btn-play-again">Play Again</button>
+        </Link>
       </>
     );
   }
