ee4b2be3581d8d21dcc2d707096e98d1533cb1b6
Juliano Boese <juliano.boese@gmail.com>
2022-03-12 16:26:53 -0300

    Estiliza somente com Material UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Button, Paper, TextField } from '@mui/material';
+import { Box, Button, Container, Paper, Stack, TextField } from '@mui/material';
 import { userLoginAction } from '../redux/actions';
-import './Login.css';
+import triviaLogo from '../assets/images/trivia-logo.gif';
 
 class Login extends Component {
   state = {
@@ -33,14 +33,30 @@ class Login extends Component {
   render() {
     const { email, name } = this.state;
     return (
-      <main>
-        <section className="image-section">
-          <div className="image-container" />
-        </section>
-        <section className="form-section">
-          <Paper elevation={ 10 } sx={ { my: 4, mx: 'auto', width: 1 / 2, py: 1, px: 4 } }>
-            <form onSubmit={ this.handleSubmit }>
-
+      <Stack
+        direction="column"
+        spacing={ 6 }
+        sx={ { bgcolor: 'rgba(255, 166, 0, 0.795)',
+          alignItems: 'center',
+          height: '100vh' } }
+      >
+        <Container sx={ { bgcolor: 'black', minWidth: '100vw' } }>
+          <Box
+            className="image-container"
+            sx={ { backgroundImage: `url(${triviaLogo})`,
+              backgroundSize: 'cover',
+              boxShadow: '25px 25px 50px 0 black inset , -25px -25px 50px 0 black inset',
+              mx: 'auto',
+              height: '26vw',
+              width: '50vw' } }
+          />
+        </Container>
+        <Paper
+          elevation={ 10 }
+          sx={ { mx: 'auto', width: 1 / 2, py: 1, px: 4 } }
+        >
+          <form onSubmit={ this.handleSubmit }>
+            <Stack direction="row" spacing={ 2 } sx={ { my: 4 } }>
               <TextField
                 id="outlined-basic"
                 label="Nome"
@@ -49,10 +65,10 @@ class Login extends Component {
                 name="name"
                 value={ name }
                 onChange={ this.handleChange }
+                sx={ { flex: 2 } }
                 inputProps={ { 'data-testid': 'input-player-name' } }
               />
               <TextField
-                className="email-input"
                 id="outlined-basic"
                 label="E-mail"
                 variant="outlined"
@@ -60,6 +76,7 @@ class Login extends Component {
                 name="email"
                 value={ email }
                 onChange={ this.handleChange }
+                sx={ { flex: 3 } }
                 inputProps={ { 'data-testid': 'input-gravatar-email' } }
               />
               <Button
@@ -68,14 +85,15 @@ class Login extends Component {
                 color="success"
                 size="large"
                 disabled={ name.length === 0 || email.length === 0 }
+                sx={ { flex: 1 } }
                 data-testid="btn-play"
               >
                 Play
               </Button>
+            </Stack>
 
-            </form>
-          </Paper>
-        </section>
+          </form>
+        </Paper>
         <Button
           component={ Link }
           to="/config"
@@ -87,7 +105,7 @@ class Login extends Component {
           Configurações
         </Button>
 
-      </main>
+      </Stack>
     );
   }
 }
